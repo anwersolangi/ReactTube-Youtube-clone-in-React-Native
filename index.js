6425df390bc2ea772e38dcaee96ecafda0f8eb46
Anwer <anwerthesolangi@gmail.com>
2021-06-22 21:55:15 +0500

    Init Project

---
+++ index.js
@@ -0,0 +1,11 @@
+/**
+ * @format
+ */
+
+import 'react-native-gesture-handler';
+
+import App from './App';
+import {AppRegistry} from 'react-native';
+import {name as appName} from './app.json';
+
+AppRegistry.registerComponent(appName, () => App);
